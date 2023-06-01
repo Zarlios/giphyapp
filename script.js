7dfd6610aa6ef319d727d01b3f084f954d72c1b3
Matthew <mdh0588@gmail.com>
2023-05-31 19:00:15 -0700

    adding search bar

--- script.js
+++ script.js
@@ -2,14 +2,16 @@ const gifContainerEl = document.querySelector('#gif-container');
 const searchboxEl = document.querySelector('#search-box');
 const searchButtonEl = document.querySelector("#search-button");
 
-console.log(searchboxEl)
-
 function getGifs() {
-    fetch('https://api.giphy.com/v1/gifs/search?api_key=hiqCK9aIGMFJKgRC3J9ozEalkC9ohp53&limit=10&rating="g"&q="kittens"')
+    let value = document.querySelector('#search-box').value;
+
+    fetch('https://api.giphy.com/v1/gifs/search?api_key=hiqCK9aIGMFJKgRC3J9ozEalkC9ohp53&limit=10&rating="g"&q="' + value + '"')
         .then((response) => response.json())
         .then(data => {
 
-            console.log(data.data);
+            // console.log(data.data);
+
+            
 
             const results = data.data;
 
@@ -26,4 +28,4 @@ function getGifs() {
         })      
 }
 
-getGifs();  
+
